24a097f3a4c7adcdcda10940797b92ddbf6ec863
小林杏理 <aanrii@aanrii-mbp-2168.local>
2019-09-17 01:09:50 +0900

    fix

--- src/components/attatchMessageServiceClient.tsx
+++ src/components/attatchMessageServiceClient.tsx
@@ -5,7 +5,7 @@ export type MessageServiceClientAttached = {
   client: MessageServiceClient;
 };
 
-const client = new MessageServiceClient(`https://34.84.218.136`);
+const client = new MessageServiceClient(`/api`);
 
 const attachMessageServiceClient = <P extends {}>(
   WrappedComponent: React.ComponentType<P & MessageServiceClientAttached>
